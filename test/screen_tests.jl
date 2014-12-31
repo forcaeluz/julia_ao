04a562cdbafdbb15451648180dd05524919bab18
forcaeluz <m.h.voorsluys@student.tudelft.nl>
2014-12-31 12:33:01 +0100

    Added screen and screen tests.

---
+++ test/screen_tests.jl
@@ -0,0 +1,36 @@
+########################################################
+# The MIT License (MIT)
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+########################################################
+
+using Base.Test
+tested_file = "../src/screen.jl"
+include("../src/screen.jl")
+
+
+function test_screen_creation()
+  print_with_color(:blue, "Testing create_centered_screen:\n")
+  screen = create_centered_screen([0.3e-3, 0.3e-3],[200, 200])
+  @test size(screen.data) == (200,200)
+end
+
+print_with_color(:green, "\nTesting ", tested_file, "\n")
+test_screen_creation()
+print_with_color(:green, tested_file, " has been tested \n")
