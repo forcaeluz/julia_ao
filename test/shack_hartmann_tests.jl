605aacf269e03136994be6def1d0239206151abc
forcaeluz <m.h.voorsluys@student.tudelft.nl>
2015-01-04 15:35:17 +0100

    Added more tests for the refactoring of the sh-sensor.

--- test/shack_hartmann_tests.jl
+++ test/shack_hartmann_tests.jl
@@ -25,21 +25,69 @@ using julia_ao
 
 tested_file = "shack_hartman.jl"
 
+#########################################################################################
+# Test functions
 function test_sensor_creation()
   print_with_color(:blue, "Testing shack hartmann sensor creation:\n")
-  config = ShackHartmannConfig(10,             # Number of lenslets in the x direction
-                              0.4e-2,
-                              200,
-                              10e-6 + 0.3e-3,
-                              0.3e-3,
-                              18e-3,
-                              630e-9,
-                              4,
-                              compute_shackhartmann_intensities,
-                              compute_shackhartmann_intensities)
-  sensor = ShackHartmannSensor(config)
+  sensor = create_sensor()
+  # To-do: Add oracles
 end
 
+function test_extract_phaseplate()
+  print_with_color(:blue, "Testing extract_phase_plate()\n")
+  sensor = create_sensor()
+  phase = create_flat_phase_screen()
+  plate = extract_phaseplate(sensor, 1, phase)
+  # To-do: Add oracles
+end
+
+function test_compute_average_phaseplate_gradients()
+  print_with_color(:blue, "Testing compute_average_phaseplate_gradients()\n")
+  sensor = create_sensor()
+  phase = create_flat_phase_screen()
+  plate = extract_phaseplate(sensor, 1, phase)
+  [gx, gy] = compute_average_phaseplate_gradients(plate)
+  #To-do: Add oracles
+end
+
+function test_compensate_phaseplate_tiptilt()
+  print_with_color(:blue, "Testing compensate_phaseplate_tiptilt!()\n")
+  sensor = create_sensor()
+  phase = create_flat_phase_screen()
+  plate = extract_phaseplate(sensor, 1, phase)
+  [gx, gy] = compute_average_phaseplate_gradients(plate)
+  compensate_phaseplate_tiptilt!(plate, gx, gy)
+  #To-do: Add oracles
+end
+
+function test_compute_imagelet_intensities()
+  print_with_color(:blue, "Testing compute_imagelet_intensities()\n")
+  sensor = create_sensor()
+  phase = create_flat_phase_screen()
+  plate = extract_phaseplate(sensor, 1, phase)
+  [gx, gy] = compute_average_phaseplate_gradients(plate)
+  compensate_phaseplate_tiptilt!(plate, gx, gy)
+  #To-do: Implement actual test.
+end
+#########################################################################################
+# Support functions
+function create_sensor()
+  config = ShackHartmannConfig(5, 0.4e-2, 200, 10e-6 + 0.3e-3, 0.3e-3, 18e-3, 630e-9, 4,
+                               compute_shackhartmann_intensities,
+                               compute_shackhartmann_intensities)
+  sensor = ShackHartmannSensor(config)
+  return sensor
+end
+
+function create_flat_phase_screen()
+
+end
+#########################################################################################
+# Test plan execution
 print_with_color(:green, "\nTesting ", tested_file, "\n")
 test_sensor_creation()
+test_extract_phaseplate()
+test_compute_average_phaseplate_gradients()
+test_compensate_phaseplate_tiptilt()
+test_compute_imagelet_intensities()
 print_with_color(:green, tested_file, " has been tested \n")
