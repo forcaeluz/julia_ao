ec7c8f73281e06cb1b8f691151951b1bbf4cf94c
forcaeluz <m.h.voorsluys@student.tudelft.nl>
2015-01-29 12:30:24 +0100

    Added private tests for the shack hartmann sensor.

---
+++ test/shack_hartmann_private.jl
@@ -0,0 +1,96 @@
+########################################################
+# The MIT License (MIT)
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+########################################################
+
+using Base.Test
+
+tested_file = "shack_hartmann.jl"
+
+include(string("../src/", tested_file))
+
+function test_extract_phaseplate()
+  print_with_color(:blue, "Testing extract_phase_plate\n")
+  sensor = create_sensor()
+  phase = create_flat_phase_screen()
+  plate = extract_phaseplate(sensor, 1, phase)
+  # Check if size of the plate is as expected.
+  @test_approx_eq plate.x_pxl_centers -7.7e-4:7.7e-6:-4.7e-4
+  @test_approx_eq plate.y_pxl_centers -7.7e-4:7.7e-6:-4.7e-4
+end
+
+function test_compute_average_phaseplate_gradients()
+  print_with_color(:blue, "Testing compute_average_phaseplate_gradients\n")
+  # Create a phase_plate with known gradient
+  phase_plate = create_screen([-1.0, -1.0], [1.0, 1.0], [0.1, 0.1])
+  for i = 1:length(phase_plate.x_pxl_centers), j = 1:length(phase_plate.y_pxl_centers)
+    phase_plate.data[i, j] = phase_plate.x_pxl_centers[i] + phase_plate.y_pxl_centers[j]
+  end
+  (gx, gy) = compute_average_phaseplate_gradients(phase_plate)
+  @test_approx_eq gx 1.0
+  @test_approx_eq gy 1.0
+end
+
+function test_compensate_phaseplate_tiptilt()
+  print_with_color(:blue, "Testing compensate_phaseplate_tiptilt!()\n")
+  phase_plate = create_screen([-1.0, -1.0], [1.0, 1.0], [0.1, 0.1])
+  for i = 1:length(phase_plate.x_pxl_centers), j = 1:length(phase_plate.y_pxl_centers)
+    phase_plate.data[i, j] = phase_plate.x_pxl_centers[i] + phase_plate.y_pxl_centers[j]
+  end
+  compensate_phaseplate_tiptilt!(phase_plate, 1.0, 1.0)
+
+  for i = 1:length(phase_plate.x_pxl_centers), j = 1:length(phase_plate.y_pxl_centers)
+    @test_approx_eq phase_plate.data[i, j] 0
+  end
+end
+
+function test_compute_imagelet_intensities()
+  print_with_color(:blue, "Testing compute_imagelet_intensities()\n")
+  #To-do: Implement actual test.
+end
+
+#########################################################################################
+# Support functions
+"""
+
+"""
+function create_sensor()
+  config = ShackHartmannConfig(5, 0.00154, 200, 10e-6 + 0.3e-3, 0.3e-3, 18e-3, 630e-9, 4,
+                               compute_shackhartmann_intensities)
+  sensor = ShackHartmannSensor(config)
+  return sensor
+end
+
+"""
+
+"""
+function create_flat_phase_screen()
+  phase_screen = create_centered_screen([0.00154, 0.00154], [200, 200])
+  return phase_screen
+end
+
+#########################################################################################
+# Test plan execution
+print_with_color(:green, "\nTesting ", tested_file, "\n")
+test_extract_phaseplate()
+test_compute_average_phaseplate_gradients()
+test_compensate_phaseplate_tiptilt()
+#test_compute_imagelet_intensities()
+print_with_color(:green, tested_file, " has been tested \n")
