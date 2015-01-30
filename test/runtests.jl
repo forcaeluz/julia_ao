4588c647ac922fd35c773cd2688b5b84f915ae85
forcaeluz <m.h.voorsluys@student.tudelft.nl>
2015-01-30 11:41:39 +0100

    Trying to fix coverage call

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,5 @@
 using Base.Test
-include("runtests_private.jl")
+#include("runtests_private.jl")
 include("deformable_mirror_tests.jl")
 include("screen_tests.jl")
 include("shack_hartmann_tests.jl")
